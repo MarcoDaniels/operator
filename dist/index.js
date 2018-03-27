83677386c82038376818336d8e8cb7e8b8e40e3a
marcodaniels <mapma@fynskemedier.dk>
2018-03-27 20:07:05 +0200

    cors and .env

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var functions = require("firebase-functions");
 var express = require("express");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 var apollo_server_express_1 = require("apollo-server-express");
 var schema_1 = require("./schema");
 var setupGraphQLServer = function () {
     var PORT = Number(process.env.PORT) || 3000;
     var server = express();
-    server.use('/graphql', bodyParser.json(), apollo_server_express_1.graphqlExpress({ schema: schema_1.schema }));
+    server.use('/graphql', cors(), bodyParser.json(), apollo_server_express_1.graphqlExpress({ schema: schema_1.schema }));
     server.listen(PORT, function () { console.log("GraphQL is up and running"); });
     return server;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
+import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
 import { graphqlExpress } from 'apollo-server-express'
 import { schema } from './schema'
@@ -8,7 +9,7 @@ const setupGraphQLServer = () => {
     const PORT = Number(process.env.PORT) || 3000
     const server = express()
 
-    server.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+    server.use('/graphql', cors(), bodyParser.json(), graphqlExpress({schema}))
     server.listen(PORT, () => { console.log(`GraphQL is up and running`) })
 
     return server
