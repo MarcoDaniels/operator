79c30a5053bba739d26716abed44ee0d67c72982
marcodaniels <mapma@fynskemedier.dk>
2018-04-06 19:17:41 +0200

    get projects

--- dist/project/data.js
+++ dist/project/data.js
@@ -1,35 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function getProject(args) {
-    return {
+var projects = [
+    {
         id: "1",
-        name: "joyful talks",
+        name: "Joyful Talks",
         homepage: "http://joyfultalks.com",
         releaseDate: "June 2016",
         description: "Joyful Talks is a personal blog",
         details: "Joyful talks run in CodeIgniter PHP, MySQL, jQuery and CSS Bootstrap."
-    };
+    },
+    {
+        id: "2",
+        name: "Cerejas Mari'José",
+        homepage: "http://marijose.pt",
+        releaseDate: "May 2014",
+        description: "Website for a fruit company",
+        details: "Cerejas Mari'José runs in Laravel PHP framework, Bootstrap CSS, MySQL and jQuery."
+    }
+];
+function getProject(args) {
+    var project = projects.filter(function (project) {
+        return project.name == args.name;
+    })[0];
+    return project;
 }
 exports.getProject = getProject;
 function getProjects() {
-    return [
-        {
-            id: "1",
-            name: "joyful talks",
-            homepage: "http://joyfultalks.com",
-            releaseDate: "June 2016",
-            description: "Joyful Talks is a personal blog",
-            details: "Joyful talks run in CodeIgniter PHP, MySQL, jQuery and CSS Bootstrap."
-        },
-        {
-            id: "2",
-            name: "Cerejas Mari'José",
-            homepage: "http://marijose.pt",
-            releaseDate: "May 2014",
-            description: "Website for a fruit company",
-            details: "Cerejas Mari'José runs in Laravel PHP framework, Bootstrap CSS, MySQL and jQuery."
-        }
-    ];
+    return projects;
 }
 exports.getProjects = getProjects;
 //# sourceMappingURL=data.js.map
--- src/project/data.ts
+++ src/project/data.ts
@@ -1,31 +1,30 @@
-export function getProject(args: any) {
-    return {
+const projects = [
+    {
         id: "1",
-        name: "joyful talks",
+        name: "Joyful Talks",
         homepage: "http://joyfultalks.com",
         releaseDate: "June 2016",
         description: "Joyful Talks is a personal blog",
         details: "Joyful talks run in CodeIgniter PHP, MySQL, jQuery and CSS Bootstrap."
+    },
+    {
+        id: "2",
+        name: "Cerejas Mari'José",
+        homepage: "http://marijose.pt",
+        releaseDate: "May 2014",
+        description: "Website for a fruit company",
+        details: "Cerejas Mari'José runs in Laravel PHP framework, Bootstrap CSS, MySQL and jQuery."
     }
+]
+
+export function getProject(args: any) {
+    const [project] = projects.filter((project) => {
+        return project.name == args.name
+    })
+
+    return project
 }
 
 export function getProjects() {
-    return [
-        {
-            id: "1",
-            name: "joyful talks",
-            homepage: "http://joyfultalks.com",
-            releaseDate: "June 2016",
-            description: "Joyful Talks is a personal blog",
-            details: "Joyful talks run in CodeIgniter PHP, MySQL, jQuery and CSS Bootstrap."
-        },
-        {
-            id: "2",
-            name: "Cerejas Mari'José",
-            homepage: "http://marijose.pt",
-            releaseDate: "May 2014",
-            description: "Website for a fruit company",
-            details: "Cerejas Mari'José runs in Laravel PHP framework, Bootstrap CSS, MySQL and jQuery."
-        }
-    ]
+    return projects
 }
