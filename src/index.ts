043247f2c844dfbfecf2dbee13f4761f8c945526
marcodaniels <mapma@fynskemedier.dk>
2018-05-28 18:51:33 +0200

    development setup

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,11 @@ import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express'
 import { schema } from './schema'
-import { printSchema } from 'graphql'
-
-let LOCAL = false
-if (process.env.NODE_ENV !== undefined) {
-    LOCAL = process.env.NODE_ENV === 'dev'
-}
-
-const PORT = Number(process.env.PORT) || 4000
-const GRAPH = '/graphql'
-const GRAPHQL = LOCAL ? GRAPH : '/operator/graphql'
-const GRAPHIQL = '/graphiql'
 
 const server = express()
 
 server.use(
-    GRAPH,
+    '/graphql',
     cors(),
     bodyParser.json(),
     graphqlExpress({
@@ -28,25 +17,10 @@ server.use(
 )
 
 server.use(
-    GRAPHIQL,
+    '/graphiql',
     graphiqlExpress({
-        endpointURL: GRAPHQL
+        endpointURL: '/operator/graphql'
     })
 )
-
-server.use(
-    '/schema',
-    (req, res) => {
-        res.set('Content-Type', 'text/plain')
-        res.send(printSchema(schema))
-    }
-)
-
-if (LOCAL) {
-    server.listen(PORT, () => {
-        console.log(`GraphQL api on http://localhost:${PORT}${GRAPH}`)
-        console.log(`GraphiQL interface on http://localhost:${PORT}${GRAPHIQL}`)
-    })
-}
 
 export const operator: HttpsFunction = https.onRequest(server)
