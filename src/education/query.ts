59e22864b53fb9402ce02dd18d20aa3801a0cfdc
Marco Martins <marcodanielmartins@gmail.com>
2019-02-05 06:11:39 +0100

    :movie_camera: Code coverage, mock database

--- src/education/query.ts
+++ src/education/query.ts
@@ -1,28 +1,12 @@
 import { GraphQLList } from 'graphql'
 import { Education } from './type'
 import { UserType } from '../user/type'
-import { dataBase } from '../database'
-
-const collection = dataBase.collection('userEducation')
-
-async function getEducations(user: UserType) {
-    const educations: any[] = []
-
-    const query = collection.where('userName', '==', user.userName)
-    await query.get()
-        .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                educations.push(doc.data())
-            })
-        })
-
-    return educations
-}
+import { getEducation } from './data'
 
 export const ListUserEducationQuery: any = {
     type: new GraphQLList(Education),
     description: Education.description,
     resolve: (source: UserType) => {
-        return getEducations(source)
+        return getEducation(source)
     }
 }
